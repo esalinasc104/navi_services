b779c78700352c2cf6846430704f0c8cf89035fd
Elias Salinas <esalinasc92@gmail.com>
2019-11-29 10:21:31 -0600

    Fixing production

--- navi_services/settings/production.py
+++ navi_services/settings/production.py
@@ -11,6 +11,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'djongo',
         'NAME': 'navi',
-        'HOST': 'ec2-34-220-174-124.us-west-2.compute.amazonaws.com:27017'
+        'HOST': 'localhost:27017'
     }
 }
